a2dea4f9bd4715757e4c4b826246d6b65ec27bd0
Jorge Luis <jorgel6m02@gmail.com>
2022-08-01 15:32:46 -0400

    ex4 completed (with animations)

--- Task 4/ex-4/main.js
+++ Task 4/ex-4/main.js
@@ -1,19 +1,33 @@
 function show(element) {
     if (element.classList.contains("show")) {
-        element.classList.remove("show");
-        element.classList.add("hidden");
+        setTimeout(() => {
+            element.classList.remove("show");
+            element.classList.add("hidden");
+        }, 500);
     } else {
         element.classList.remove("hidden");
         element.classList.add("show");
     }
 }
 
-document.querySelector(".burger").addEventListener("click", () => {
+function animateMenu(element) {
+    if (element.classList.contains("show-menu")) {
+        element.classList.remove("show-menu");
+        element.classList.add("hide-menu");
+    } else {
+        element.classList.remove("hide-menu");
+        element.classList.add("show-menu");
+    }
+}
+
+document.querySelector(".burger").addEventListener("click", (e) => {
     let menuContainer = document.querySelector("#menu-wrapper").parentElement;
+    animateMenu(menuContainer)
     show(menuContainer);
 })
 
 document.querySelector(".show-child-menu").addEventListener("click", (e) => {
     let childMenu = e.target.nextElementSibling;
     show(childMenu);
+    animateMenu(childMenu);
 })
